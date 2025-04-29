58e7e39892a859ca10bc12710aea7314ca98430e
izaack107 <izaack107@gmail.com>
2025-04-28 21:04:16 -0600

    Using plugins from the creation of the image

--- user-data/jenkins-user-data.sh
+++ user-data/jenkins-user-data.sh
@@ -32,6 +32,68 @@ sudo apt-get install -y docker-ce=$VERSION_STRING docker-ce-cli=$VERSION_STRING 
 sudo systemctl enable docker
 sudo systemctl restart docker
 
+cat <<EOF | sudo tee /home/ubuntu/plugins.txt
+# Core
+configuration-as-code:1958.vddc0d369b_e16
+
+# Build tools
+ant
+gradle
+
+# Utilities
+build-timeout
+durable-task
+email-ext
+instance-identity
+mailer
+antisamy-markup-formatter
+plain-credentials
+resource-disposer
+ws-cleanup
+timestamper
+
+# SCM
+git
+git-client
+gitlab-plugin
+github
+github-branch-source
+
+# SSH agents (optional)
+ssh-slaves
+
+# Azure
+azure-credentials
+azure-cli
+azure-keyvault
+azure-sdk
+
+# Testing
+junit
+
+# Credentials
+credentials
+credentials-binding
+ssh-credentials
+
+# Security
+ldap
+matrix-auth
+pam-auth
+script-security
+
+# UI
+dark-theme
+theme-manager
+
+# Pipeline umbrella
+workflow-aggregator
+
+# Organization
+cloudbees-folder
+matrix-project
+
+EOF
 
 # Create Dockerfile file for Jenkins
 cat <<EOF | sudo tee /home/ubuntu/Dockerfile
@@ -39,20 +101,16 @@ cat <<EOF | sudo tee /home/ubuntu/Dockerfile
 FROM jenkins/jenkins:lts-jdk17
 
 USER root
-RUN apt-get update \
- && apt-get install -y wget unzip openssh-client \
- && wget https://releases.hashicorp.com/terraform/1.7.2/terraform_1.7.2_linux_amd64.zip \
- && unzip terraform_1.7.2_linux_amd64.zip -d /usr/local/bin \
- && rm terraform_1.7.2_linux_amd64.zip \
- && curl -sL https://aka.ms/InstallAzureCLIDeb | bash \
- && rm -rf /var/lib/apt/lists/*
+RUN apt-get update  && apt-get install -y wget unzip openssh-client  && wget https://releases.hashicorp.com/terraform/1.7.2/terraform_1.7.2_linux_amd64.zip  && unzip terraform_1.7.2_linux_amd64.zip -d /usr/local/bin  && rm terraform_1.7.2_linux_amd64.zip  && curl -sL https://aka.ms/InstallAzureCLIDeb | bash  && rm -rf /var/lib/apt/lists/*
 
 # Switch back to the jenkins user for plugin install
 USER jenkins
 
 # Copy plugin list and install
-#COPY --chown=jenkins:jenkins plugins.txt /usr/share/jenkins/ref/plugins.txt
-#RUN jenkins-plugin-cli -f /usr/share/jenkins/ref/plugins.txt
+COPY --chown=jenkins:jenkins plugins.txt /usr/share/jenkins/ref/plugins.txt
+RUN jenkins-plugin-cli --plugin-file /usr/share/jenkins/ref/plugins.txt
+
+
 EOF
 
 # Create docker-compose.yml file for Jenkins
@@ -64,13 +122,14 @@ services:
       dockerfile: Dockerfile
     container_name: jenkins
     user: root
+    environment:
+      - JAVA_OPTS=-Djenkins.install.runSetupWizard=false -Djava.util.logging.config.file=/var/jenkins_home/log.properties
+      - CASC_JENKINS_CONFIG=/var/jenkins_home/jenkins.yaml
     ports:
       - '8080:8080'
-    environment:
-     
-
     volumes:
       - jenkins-vol:/var/jenkins_home
+      - ./jenkins.yaml:/var/jenkins_home/jenkins.yaml:ro 
 
 volumes:
   jenkins-vol:
